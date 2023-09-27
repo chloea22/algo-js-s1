7c9dd7dd0571bb4c1a90818fe3a4f6b16def719f
chloe arnolin <146062274+chloea22@users.noreply.github.com>
2023-09-27 20:29:16 +0200

    Add files via upload

---
+++ Exercice 7/main.js
@@ -0,0 +1,10 @@
+let Character = {
+    firstName : "chloe",
+    lastName : " arnolin",
+    age : 18,
+}
+
+let sentence = " " + Character.firstName + Character.lastName + " a " + Character.age
+
+console.log(sentence)
+
